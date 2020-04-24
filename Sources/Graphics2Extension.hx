5affd02f4392828faa44375d2f12dfb8b5ad26ba
carlito767 <charles.longuemart@gmail.com>
2020-04-24 17:31:37 +0200

    Minor changes

--- Sources/Graphics2Extension.hx
+++ Sources/Graphics2Extension.hx
@@ -1,10 +1,9 @@
-import kha.FastFloat;
 import kha.System;
 
 class Graphics2Extension {
-  public static function centerString(g2:kha.graphics2.Graphics, s:String, y:FastFloat, dx:FastFloat = 0.0):Void {
+  public static function centerString(g2:kha.graphics2.Graphics, s:String, y:Float, dx:Float = 0.0):Void {
     var w = g2.font.width(g2.fontSize, s);
-    var x = (System.windowWidth() - w) / 2;
+    var x = (System.windowWidth() - w) * 0.5;
     g2.drawString(s, x + dx, y);
   }
 }
