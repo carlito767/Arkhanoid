a449f77b4f17f2a54edbeeb59eccb331e76a2e78
carlito767 <charles.longuemart@gmail.com>
2020-04-26 16:56:22 +0200

    Detect collision between ball and edges

--- Sources/rounds/Round.hx
+++ Sources/rounds/Round.hx
@@ -10,11 +10,18 @@ import sprites.Edge;
 import sprites.Paddle;
 import sprites.Sprite;
 
+import Math.PI;
+
 typedef Bounds = {
   left:Float,
   top:Float,
   right:Float,
   bottom:Float,
+}
+
+typedef Point = {
+  x:Float,
+  y:Float,
 }
 
 class Round {
@@ -106,6 +113,20 @@ class Round {
         ball.x += dx;
       }
       else {
+        var collisions = new List<Bounds>();
+
+        // Detect collision between ball and edges
+        for (edge in [edgeLeft, edgeRight, edgeTop]) {
+          if (collide(ball, edge)) {
+            collisions.add(bounds(edge));
+          }
+        }
+
+        // Determine new angle for ball
+        if (!collisions.isEmpty()) {
+          newAngleForBall(ball, collisions);
+        }
+
         ball.x += ball.speed * Math.cos(ball.angle);
         ball.y += ball.speed * Math.sin(ball.angle);
       }
@@ -153,6 +174,118 @@ class Round {
   // Collisions
   //
 
+  static var HALF_PI(get,never):Float; static inline function get_HALF_PI() { return PI * 0.5; };
+  static var TWO_PI(get,never):Float; static inline function get_TWO_PI() { return PI * 2; };
+
+  // A value will be chosen at random between this and it's negative
+  // to apply to the angle of bounce for top/bottom/side collisions of the ball
+  static inline var RANDOM_RANGE = 0.1; // Radians
+
+  function newAngleForBall(ball:Ball, collisions:List<Bounds>):Void {
+    if (collisions.isEmpty()) return;
+
+    var angle = ball.angle;
+
+    // Determine collide points
+    var tl = false;
+    var tr = false;
+    var bl = false;
+    var br = false;
+
+    var bb = bounds(ball);
+    for (collision in collisions) {
+      tl = tl || isInside(collision, {x:bb.left, y:bb.top});
+      tr = tr || isInside(collision, {x:bb.right, y:bb.top});
+      bl = bl || isInside(collision, {x:bb.left, y:bb.bottom});
+      br = br || isInside(collision, {x:bb.right, y:bb.bottom});
+    }
+
+    var collideTrue = [tl, tr, bl, br].filter((v)->{v == true;});
+    if (collideTrue.length == 1) {
+      if (tl) {
+        if (angle > TWO_PI - HALF_PI)
+          tr = true;
+        else if (angle < PI) {
+          bl = true;
+        }
+      }
+      else if (tr) {
+        if (angle >= PI && angle <= TWO_PI - HALF_PI) {
+          tl = true;
+        }
+        else if (angle < HALF_PI) {
+          br = true;
+        }
+      }
+      else if (bl) {
+        if (angle < HALF_PI) {
+          br = true;
+        }
+        else if (angle > PI) {
+          tl = true;
+        }
+      }
+      else if (br) {
+        if (angle <= PI && angle > HALF_PI) {
+          bl = true;
+        }
+        else if (angle > TWO_PI - HALF_PI) {
+          tr = true;
+        }
+      }
+    }
+
+    // Determine new angle
+    switch (collideTrue.length) {
+      case 1 | 3 | 4:
+        angle += (angle > PI) ? -PI : PI;
+        if (collideTrue.length == 1) {
+          // Add some randomness to corner collisions to prevent bounce
+          // loops
+          angle += RANDOM_RANGE - (2 * RANDOM_RANGE * Math.random());
+        }
+      case _:
+        var topCollision = tl && tr && angle > PI;
+        var bottomCollision = bl && br && angle < PI;
+        if (topCollision || bottomCollision) {
+          angle = TWO_PI - angle;
+          // Prevent vertical bounce loops by detecting near vertical
+          // angles and adjusting the angle of bounce
+          if (angle > (TWO_PI - HALF_PI - 0.06) && angle < (TWO_PI - HALF_PI + 0.06)) {
+            angle += 0.35;
+          }
+          else if (angle < (HALF_PI + 0.06) && angle > (HALF_PI - 0.06)) {
+            angle += 0.35;
+          }
+        }
+        else {
+          var leftCollision = tl && bl && angle > HALF_PI && angle < (TWO_PI - HALF_PI);
+          var rightCollision = tr && br && (angle > (TWO_PI - HALF_PI) || angle < HALF_PI);
+          if (leftCollision || rightCollision) {
+            angle = (angle < PI) ? (PI - angle) : TWO_PI - angle + PI;
+            // Prevent horizontal bounce loops by detecting near
+            // horizontal angles and adjusting the angle of bounce
+            if (angle > (PI - 0.06) && angle < (PI + 0.06)) {
+              angle += 0.35;
+            }
+            else if (angle > TWO_PI - 0.06) {
+              angle -= 0.35;
+            }
+            else if (angle < 0.06) {
+              angle += 0.35;
+            }
+          }
+
+          // Add a small amount of randomness to the bounce to make it a
+          // little more unpredictable, and to prevent the ball from getting
+          // stuck in a repeating bounce loop
+          angle += RANDOM_RANGE - (2 * RANDOM_RANGE * Math.random());
+        }
+    }
+
+    ball.angle = angle;
+  }
+
   function bounds(sprite:Sprite, ?dx:Float = 0, ?dy:Float = 0):Bounds {
     return {
       left:sprite.x + dx,
@@ -164,6 +297,10 @@ class Round {
 
   function collide(spriteA:Sprite, spriteB:Sprite, ?dx:Float = 0, ?dy:Float = 0):Bool {
     return isIntersecting(bounds(spriteA, dx, dy), bounds(spriteB));
+  }
+
+  function isInside(A:Bounds, point:Point):Bool {
+    return point.x >= A.left && point.x <= A.right && point.y >= A.top && point.y <= A.bottom;
   }
 
   inline function isIntersecting(A:Bounds, B:Bounds) {
