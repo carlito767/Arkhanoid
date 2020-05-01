749dcd98f41129bc7d1c82387e50f5de6f75a5fe
carlito767 <charles.longuemart@gmail.com>
2020-05-01 09:10:51 +0200

    Load rounds from JSON files

--- Sources/rounds/RoundsBuilder.hx
+++ Sources/rounds/RoundsBuilder.hx
@@ -1,31 +1,43 @@
 package rounds;
 
 import kha.Assets;
-import kha.Color;
 
 import sprites.Brick;
 
 typedef RawRound = {
-  backgroundColor:Color,
+  backgroundColor:Int,
   ?ballBaseSpeedAdjust:Float,
   ?ballSpeedNormalisationRateAdjust:Float,
   bricks:Array<String>,
 }
 
 class RoundsBuilder {
-  static final ROUNDS:Array<Void->RawRound> = [
-    round1,
-    round2,
-    round3,
-    round4,
-    round5,
-  ];
-
   public static function rounds() {
     var rounds:Array<RoundDataFactory> = [];
-    for (round in ROUNDS) {
-      rounds.push(()->{ return cook(round()); });
+
+    // Load rounds from JSON files (Assets/round*.json)
+    var i = 1;
+    while (true) {
+      var blob = Assets.blobs.get('round${i}_json');
+      if (blob == null) break;
+      trace('Loading round $i...');
+
+      // TODO: check data
+      var data = null;
+      try {
+        data = haxe.Json.parse(blob.toString());
+      }
+      catch (e:Dynamic) {
+        trace('Parsing error: $e');
+        break;
+      }
+      if (data == null) break;
+
+      rounds.push(()->{ return cook(data); });
+
+      i++;
     }
+
     return rounds;
   }
 
@@ -67,125 +79,6 @@ class RoundsBuilder {
       ballBaseSpeedAdjust:rawRound.ballBaseSpeedAdjust,
       ballSpeedNormalisationRateAdjust:rawRound.ballSpeedNormalisationRateAdjust,
       bricks:bricks
-    };
-  }
-
-  static function round1():RawRound {
-    return {
-      backgroundColor:Color.fromBytes(0, 0, 128),
-      bricks:[
-        '',
-        '',
-        '',
-        '',
-        'SSSSSSSSSSSSS',
-        'RRRRRRRRRRRRR',
-        'YYYYYYYYYYYYY',
-        'BBBBBBBBBBBBB',
-        'PPPPPPPPPPPPP',
-        'GGGGGGGGGGGGG',
-      ],
-    };
-  }
-
-  static function round2():RawRound {
-    return {
-      backgroundColor:Color.fromBytes(0, 128, 0),
-      bricks:[
-        '',
-        '',
-        'W',
-        'WO',
-        'WOC',
-        'WOCG',
-        'WOCGR',
-        'WOCGRB',
-        'WOCGRBP',
-        'WOCGRBPY',
-        'WOCGRBPYW',
-        'WOCGRBPYWO',
-        'WOCGRBPYWOC',
-        'WOCGRBPYWOCG',
-        'SSSSSSSSSSSSR',
-      ],
-    };
-  }
-
-  static function round3():RawRound {
-    return {
-      backgroundColor:Color.fromBytes(0, 0, 128),
-      ballBaseSpeedAdjust:-2.0,
-      ballSpeedNormalisationRateAdjust:0.05,
-      bricks:[
-        '',
-        '',
-        '',
-        'GGGGGGGGGGGGG',
-        '',
-        'WWW**********',
-        '',
-        'RRRRRRRRRRRRR',
-        '',
-        '**********WWW',
-        '',
-        'PPPPPPPPPPPPP',
-        '',
-        'BBB**********',
-        '',
-        'CCCCCCCCCCCCC',
-        '',
-        '**********CCC',
-      ],
-    };
-  }
-
-  static function round4():RawRound {
-    return {
-      backgroundColor:Color.fromBytes(128, 0, 0),
-      bricks:[
-        '',
-        '',
-        '',
-        ' OCGSB YWOCG',
-        ' CGSBP WOCGS',
-        ' GSBPY OCGSB',
-        ' SBPYW CGSBP',
-        ' BPYWO GSBPY',
-        ' PYWOC SBPYW',
-        ' YWOCG BPYWO',
-        ' WOCGS PYWOC',
-        ' OCGSB YWOCG',
-        ' CGSBP WOCGS',
-        ' GSBPY OCGSB',
-        ' SBPYW CGSBP',
-        ' BPYWO GSBPY',
-        ' PYWOC SBPYW',
-      ],
-    };
-  }
-
-  static function round5():RawRound {
-    return {
-      backgroundColor:Color.fromBytes(0, 0, 128),
-      bricks:[
-        '',
-        '   Y     Y',
-        '   Y     Y',
-        '    Y   Y',
-        '    Y   Y',
-        '   SSSSSSS',
-        '   SSSSSSS',
-        '  SSRSSSRSS',
-        '  SSRSSSRSS',
-        ' SSSSSSSSSSS',
-        ' SSSSSSSSSSS',
-        ' SSSSSSSSSSS',
-        ' S SSSSSSS S',
-        ' S S     S S',
-        ' S S     S S',
-        '    SS SS',
-        '    SS SS',
-      ],
     };
   }
 }
