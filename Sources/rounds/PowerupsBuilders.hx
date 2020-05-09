32b86cc033e72d1d155c8c8035f27fbf3803ee66
carlito767 <charles.longuemart@gmail.com>
2020-05-09 23:53:53 +0200

    Test all powerups

--- Sources/rounds/PowerupsBuilders.hx
+++ Sources/rounds/PowerupsBuilders.hx
@@ -4,17 +4,32 @@ import Random;
 import components.Brick;
 import components.PowerupType;
 
+typedef Bricks = Array<Brick>;
+
 class PowerupBuilders {
   static final POWERUP_TYPES = Type.allEnums(PowerupType);
 
-  public static function fullRandom(bricks:Array<Brick>):Void {
+  public static function fullPowerup(bricks:Bricks, powerupType:PowerupType):Void {
     for (brick in bricks) {
-      var powerType = Std.random(POWERUP_TYPES.length);
-      brick.powerupType = POWERUP_TYPES[powerType];
+      brick.powerupType = powerupType;
     }
   }
 
-  public static function round1(bricks:Array<Brick>):Void {
+  public static function fullPowerupCatch(bricks:Bricks):Void { fullPowerup(bricks, Catch); }
+  public static function fullPowerupDuplicate(bricks:Bricks):Void { fullPowerup(bricks, Duplicate); }
+  public static function fullPowerupExpand(bricks:Bricks):Void { fullPowerup(bricks, Expand); }
+  public static function fullPowerupLaser(bricks:Bricks):Void { fullPowerup(bricks, Laser); }
+  public static function fullPowerupLife(bricks:Bricks):Void { fullPowerup(bricks, Life); }
+  public static function fullPowerupSlow(bricks:Bricks):Void { fullPowerup(bricks, Slow); }
+
+  public static function fullRandom(bricks:Bricks):Void {
+    for (brick in bricks) {
+      var powerupType = Std.random(POWERUP_TYPES.length);
+      brick.powerupType = POWERUP_TYPES[powerupType];
+    }
+  }
+
+  public static function round1(bricks:Bricks):Void {
     var p:Array<PowerupType> = [
       Catch, Catch, Catch,
       Expand, Expand, Expand, Expand,
@@ -31,7 +46,7 @@ class PowerupBuilders {
     }
   }
 
-  public static function round2(bricks:Array<Brick>):Void {
+  public static function round2(bricks:Bricks):Void {
     // Create slow ball and catch for the bottom row (except red brick),
     // given the lack of space beneath
     var pBottom:Array<PowerupType> = [ Catch, Catch, Slow, Slow ];
@@ -56,7 +71,7 @@ class PowerupBuilders {
     }
   }
 
-  public static function round3(bricks:Array<Brick>):Void {
+  public static function round3(bricks:Bricks):Void {
     bricks[31].powerupType = Duplicate;
     bricks[56].powerupType = Duplicate;
     bricks[60].powerupType = Life;
@@ -65,7 +80,7 @@ class PowerupBuilders {
     bricks[101].powerupType = Life;
   }
 
-  public static function round4(bricks:Array<Brick>):Void {
+  public static function round4(bricks:Bricks):Void {
     bricks[6].powerupType = Duplicate;
     bricks[10].powerupType = Duplicate;
     bricks[31].powerupType = Catch;
@@ -76,7 +91,7 @@ class PowerupBuilders {
     bricks[115].powerupType = Expand;
   }
 
-  public static function round5(bricks:Array<Brick>):Void {
+  public static function round5(bricks:Bricks):Void {
     var p:Array<PowerupType> = [
       Catch, Catch, Catch,
       Duplicate, Duplicate,
